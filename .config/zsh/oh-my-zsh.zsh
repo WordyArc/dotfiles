c8289b9f0309600bf4ecdaa52a620d72ec8e4045
Viktor Kogai <ashenarx@gmail.com>
2026-07-20 07:53:11 +1000

    Add omz dotenv conf

--- .config/zsh/oh-my-zsh.zsh
+++ .config/zsh/oh-my-zsh.zsh
@@ -15,6 +15,10 @@ plugins=(
 
 export ZSH_CACHE_DIR="$XDG_CACHE_HOME/oh-my-zsh"
 export ZSH_COMPDUMP="$ZSH_CACHE_DIR/.zcompdump-$ZSH_VERSION"
+
+export ZSH_DOTENV_ALLOWED_LIST="$XDG_STATE_HOME/zsh/dotenv-allowed.list"
+export ZSH_DOTENV_DISALLOWED_LIST="$XDG_STATE_HOME/zsh/dotenv-disallowed.list"
+
 mkdir -p "$ZSH_CACHE_DIR"
 
 if [[ -r "$ZSH/oh-my-zsh.sh" ]]; then
